b8d11cc5efc5b4f61c9920b70e70aff7ef343a1a
manoj010 <manojale100@gmail.com>
2022-08-18 22:15:38 +0545

    Assignment-2

---
+++ Assignment-2/Problem-2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problem-2</title>
+</head>
+<body>
+    <?php
+
+    function evaluate($name, $attendance, $hygiene, $rating) {
+        if ($attendance < 80 && $hygiene < 50) {
+            echo "$name, You are fired from Rojan Kirana Pasal";
+        }
+
+        elseif ($hygiene < 50 && $rating < 2) {
+            echo "$name, You are fired from Rojan Kirana Pasal";
+        }
+
+        else { 
+            echo "Not fired for now.";
+        }
+    }
+
+    evaluate("Kushal Karki", 90, 70, 3);
+
+    ?>
+</body>
+</html>
